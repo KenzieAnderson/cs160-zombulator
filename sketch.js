2c44a48ec9b3071b9ba05f90e6088cb313d19bc7
Kenzie Anderson <kenzieishere@gmail.com>
2017-10-04 09:51:22 -0700

    Made circles move

--- sketch.js
+++ sketch.js
@@ -1,12 +1,18 @@
 //Zombulator by Kenzie Anderson 
 
+var zombieX = 50;
+var zombie2X = 100;
+
 function setup() {
 	createCanvas(800,800);
 }
 
 function draw() {
+	background(255,255,255)
 	fill(255,0,0);
-	ellipse(50, 50, 80, 80);
+	ellipse(zombieX, 50, 80, 80);
 	fill(150,150,200);
-	ellipse(200,100,80,80);
+	ellipse(zombie2X,100,80,80);
+	zombieX = zombieX + 1; 
+	zombie2X = zombie2X + 3; 
 }
