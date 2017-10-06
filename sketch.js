e5b31312a3ade93cd30df724d9462c7b53cbeafd
Kenzie Anderson <kenzieishere@gmail.com>
2017-10-06 09:34:22 -0700

    add "if then" command for circle to repeat

--- sketch.js
+++ sketch.js
@@ -13,6 +13,14 @@ function draw() {
 	ellipse(zombieX, 50, 80, 80);
 	fill(150,150,200);
 	ellipse(zombie2X,100,80,80);
-	zombieX = zombieX + 1; 
+	zombieX = zombieX + 5; 
 	zombie2X = zombie2X + 3; 
+	if (zombieX >= 800) {
+		zombieX =0;
+	}
+	if (zombie2X >= 800) {
+		zombie2X =0;
+	}
+
+
 }
