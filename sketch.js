fb7177677f675837b6b789e35b706e8800758b08
Kenzie Anderson <kenzieishere@gmail.com>
2017-10-06 09:54:30 -0700

    change stroke of circles

--- sketch.js
+++ sketch.js
@@ -10,8 +10,12 @@ function setup() {
 function draw() {
 	background(255,255,255)
 	fill(255,0,0);
+	strokeWeight(3);
+	stroke(255,0,0);
 	ellipse(zombieX, 50, 80, 80);
 	fill(150,150,200);
+	strokeWeight(10);
+	stroke(240,150,60);
 	ellipse(zombie2X,100,80,80);
 	zombieX = zombieX + 5; 
 	zombie2X = zombie2X + 3; 
@@ -21,6 +25,5 @@ function draw() {
 	if (zombie2X >= 800) {
 		zombie2X =0;
 	}
-
 
 }
