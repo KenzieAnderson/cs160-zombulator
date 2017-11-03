5b1c9031e1d45c1157e4b0e15dbc70691b7db79c
Kenzie Anderson <kenzieishere@gmail.com>
2017-11-03 09:20:46 -0700

    Added text to zombie and human count

--- sketch.js
+++ sketch.js
@@ -2,25 +2,17 @@ var backgroundColor;
 
 const MIN_SIZE = 5;
 const MAX_SIZE = 50;
-const POPULATION_SIZE = 500
+const POPULATION_SIZE = 500;
 
-var population = []; 
+var population = [];
+
+var zombieCount = 0;
+var humanCount = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   backgroundColor = color(245, 255, 245);
-  initializePopulation(); 
-}
-
-function initializePopulation(){ 
-  for (var i = 0; i < POPULATION_SIZE; ++i) {
-    var humanoid_type = random(0,100);
-    if (humanoid_type <= 50) {
-      population[i] = initializeZombie();
-    } else {
-      population[i] = initializeHuman(); 
-    }
-  }
+  initializePopulation();
 }
 
 function draw() {
@@ -28,8 +20,29 @@ function draw() {
   noStroke();
   drawPopulation();
   movePopulation();
+  drawPopulationCounts();
 }
 
+function initializePopulation() {
+  for (var i = 0; i < POPULATION_SIZE; ++i) {
+    var humanoid_type = random(0, 100);
+    if (humanoid_type <= 50) {
+      population[i] = initializeZombie();
+      ++zombieCount;
+    } else {
+      population[i] = initializeHuman();
+      ++humanCount;
+    }
+  }
+}
+
+function drawPopulationCounts() {
+  stroke(0);
+  textSize(72);
+  textAlign(CENTER);
+  text("Zombies: " + zombieCount, width / 2, 100);
+  text("Humans: " + humanCount, width / 2, height - 100);
+}
 
 function drawPopulation() {
   for (var i = 0; i < POPULATION_SIZE; ++i) {
@@ -38,11 +51,10 @@ function drawPopulation() {
 }
 
 function movePopulation() {
-  for(var i = 0; i < POPULATION_SIZE; ++i) {
+  for (var i = 0; i < POPULATION_SIZE; ++i) {
     population[i].move();
   }
 }
-
 
 function initializeZombie() {
   return {
@@ -70,29 +82,28 @@ function initializeZombie() {
   };
 }
 
-
-function initializeHuman(index) {
+function initializeHuman() {
   return {
     x: random(0, windowWidth),
     y: random(windowHeight - 200, windowHeight),
     speed: random(0.25, 3),
     size: random(MIN_SIZE, MAX_SIZE),
     color: color(random(50, 150), random(50, 150), random(150, 255), 150),
-    draw: function() {
-      fill(this.color);
-      ellipse(this.x, this.y, this.size, this.size);
-    },
     move: function() {
-      var direction = random(0, 100);
-      if (direction < 20) {
-        this.x += this.speed;
-      } else if (direction < 40) {
-        this.x -= this.speed;
-      } else if (direction < 60) {
-        this.y += this.speed;
-      } else {
-        this.y -= this.speed;
-      }
+        var direction = random(0, 100);
+        if (direction < 20) {
+          this.x += this.speed;
+        } else if (direction < 40) {
+          this.x -= this.speed;
+        } else if (direction < 60) {
+          this.y += this.speed;
+        } else {
+          this.y -= this.speed;
+        }
+      },
+    draw: function() {
+        fill(this.color);
+        ellipse(this.x, this.y, this.size, this.size);
     }
-  }
+  };
 }
